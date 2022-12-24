c7fdf3fe958763486b63ae065befa7531417ea0d
Alexandru Carp <105707690+carpuplay@users.noreply.github.com>
2022-12-24 22:50:42 +0000

    carpu code is working, mady some optimisation can be done

--- carpu.py
+++ carpu.py
@@ -1,17 +1,34 @@
 from PIL import Image
 
 #we create the function
-def blur(org_file, dest_file, blur_x, blur_y):
+def blur(org_file, dest_file, blur_x, blur_y, pos_x, pos_y):
     org_img = Image.open(org_file)
     height, width = org_img.size
     dest_img = Image.new("RGB",(height, width))
 
-    for y in range(blur_y):
-        for x in range(blur_x):
-            r, g, b = org_img.getpixel((x,y))
-            c = int((r+g+b)/3)
-            dest_img.putpixel((x,y),(c,c,c))
+    for y in range(height):
+        for x in range(width):
+
+            if (x > pos_x) and (x < (pos_x + blur_x)) and (y > pos_y) and (y < (pos_y + blur_y)):
+                r_tmp = []
+                g_tmp = []
+                b_tmp = []
+                for y1 in range(15):
+                    for x1 in range(15):
+                        r, g, b = org_img.getpixel((x + x1 ,y + y1))
+                        r_tmp.append(r)
+                        g_tmp.append(g)
+                        b_tmp.append(b)
+                r = int(sum(r_tmp) / len(r_tmp))
+                g = int(sum(g_tmp) / len(g_tmp))
+                b = int(sum(b_tmp) / len(b_tmp))
+                dest_img.putpixel((x,y),(r, g, b))
+            
+            else:
+                r, g, b = org_img.getpixel((x, y))
+                dest_img.putpixel((x, y),(r, g, b))
+    
     dest_img.save(dest_file)
     dest_img.show()
 
-blur('cat.PNG', 'new.PNG', 215, 300)
+blur('cat.PNG', 'new.PNG', 215, 300, 100, 250)
